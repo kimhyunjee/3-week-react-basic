83dc8a91e99f62475dc0f1a4146b682c46782706
내꺼다\ysk30 <ysk30430@naver.com>
2022-07-27 21:59:29 +0900

    header component separation

---
+++ src/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"; //리액트사용기본
+import "./style.css";       //style.css임포트
+
+function Header(){
+    return (
+        
+        <div className="container">
+            <div>My Todo List</div>
+            <div>React</div>
+        </div>
+
+    ); 
+}
+
+export default Header;
